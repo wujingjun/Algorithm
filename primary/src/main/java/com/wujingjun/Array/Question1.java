cce54a5654317855b7e04208c1af8325041a8162
JingJun.Wu <wujingjun@kinthtech.com>
2022-06-09 00:07:44 +0800

    删除有序数组的重复项问题解决

--- primary/src/main/java/com/wujingjun/Array/Question1.java
+++ primary/src/main/java/com/wujingjun/Array/Question1.java
@@ -1,2 +1,24 @@
-package com.wujingjun.Array;public class Question1 {
+package com.wujingjun.Array;
+
+
+public class Question1 {
+
+    public static void main(String[] args) {
+        Question1 question1 = new Question1();
+        int i = question1.removeDuplicates(new int[]{1, 1, 2, 2, 3, 4, 5, 5});
+        System.out.println(i);
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        int record = nums[index];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != record) {
+                index++;
+                nums[index] = nums[i];
+                record = nums[i];
+            }
+        }
+        return index+1;
+    }
 }
